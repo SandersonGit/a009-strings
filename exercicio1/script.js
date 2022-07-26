ea22c7aeeada0eb430f24ec43ea63b73d1713395
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-07-25 22:11:55 -0300

    Barbsa-Sanderson-Luiz

---
+++ exercicio1/script.js
@@ -0,0 +1,6 @@
+const nome = prompt("Qual o seu nome?")
+const comida = prompt("Qual sua comida favorita?")
+const bebida = prompt("Qual sua bebida favorita?")
+const sobremesa = prompt("Qual sua sobremesa favorita?")
+
+console.log(`Estas são a comida, bebida e sobremesa \nfavoritas de ${nome}, respectivamente: \n- ${comida} \n- ${bebida} \n- ${sobremesa} `);
